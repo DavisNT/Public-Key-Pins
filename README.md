6213209c055f837cafb930e1d99483affa8e1815
Davis Mosenkovs <davikovs@gmail.com>
2014-06-09 00:18:31 +0300

    Before final adjustments

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 JavaScript Public-Key-Pins calculator
 ===============
+JavaScript Public-Key-Pins calculator - JavaScript library for easy calculation of public key hashes for use in 
+Public Key Pinning Extension for HTTP.
+
+Copyright (C) 2014 Davis Mosenkovs
 
 ## Introduction
 
@@ -13,6 +17,21 @@ JavaScript Public-Key-Pins calculator allows easy calculation of Public-Key-Pins
 It can be used as offline HTML/JavaScript form or embeded into web site (using API provided by `pkps.js`).
 
 ## Notices
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+http://www.gnu.org/copyleft/gpl.html
 
 This project uses [Forge](https://github.com/digitalbazaar/forge) library which is distributed under the 
 terms of [either the BSD License or the GNU General Public License (GPL) Version 2](https://github.com/digitalbazaar/forge/blob/master/LICENSE).
--- pkps.js
+++ pkps.js
@@ -1,5 +1,6 @@
 /* JavaScript Public-Key-Pins calculator - JavaScript library for easy 
  * calculation of public key hashes for use in Public Key Pinning Extension for HTTP.
+ * Version 0.9.9
  * Copyright (C) 2014 Davis Mosenkovs
  *
  * This program is free software; you can redistribute it and/or
@@ -18,16 +19,160 @@
  * http://www.gnu.org/copyleft/gpl.html
  */
 
-// namespace definition
+/** Namespace definition */
 var pkps = pkps || {};
 
 /**
- * Generates public key hashes for specified certificate or CSR in PEM format.
+ * Generates public key hashes for specified set of certificates and/or CSRs.
+ * This is the object of this library that will most likely be used externally.
+ * Currently only RSA keys are supported.
+ * Mandatory selftests are invoked automatically each time object of this type is created.
+ *
+ * @param certificatesAndCsrs newline or space separated certificates and/or certificate signing requests in PEM format.
+ * @return publicKeyPinsSet object with set of the hashes.
+ *   formatReport(), hasWarnings(), formatWarnings() and formatPKPHeader() methods should be used on this object.
+ * @throws string with textual description of failure.
+ */
+pkps.publicKeyPinsSet = function(certificatesAndCsrs) {
+    this.pins = new Array();
+    this.inputs = new Array();
+    this.warnings = new Array();
+
+    var pems = null;
+    var i, j;
+    var pi_nondns = false;
+    var pi_diffcn = false;
+    var pi_samekey = false;
+    var validHostnameRegex = /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/; // credits to http://stackoverflow.com/questions/106179
+
+    // invoke selftests
+    pkps.doSelfTests();
+
+    // split input
+    pems = certificatesAndCsrs.split("-----BEGIN ");
+    if(pems.length<2 || pems[0].trim()!=="")
+        throw "Unable to split input into PEM certificates/CSRs.";
+
+    // generate Public-Key-Pins
+    for(i=1; i<pems.length; i++) {
+        this.pins.push(new pkps.publicKeyPin("-----BEGIN "+pems[i].trim()));
+        this.inputs.push("-----BEGIN "+pems[i].trim());
+    }
+
+    // detect possible issues
+    for(i=0; i<this.pins.length; i++) {
+        // different common names
+        if(this.pins[i].getCN()!==this.pins[0].getCN())
+            pi_diffcn = true;
+        // common names that are not hostnaames
+        if(this.pins[i].getCN().match(validHostnameRegex)==null)
+            pi_nondns = true;
+        // duplicate public keys
+        for(j=0; j<i; j++) {
+            if(this.pins[i].getPublicKeyPem()==this.pins[j].getPublicKeyPem())
+                pi_samekey = true;
+        }
+    }
+    if(pi_diffcn)
+        this.warnings.push("Different Common Names (CNs) detected. Don't continue unless you really know what you are doing! Don't paste full certificate chain (CA bundle) here!");
+    if(pi_nondns)
+        this.warnings.push("Not all Common Names (CNs) are host names (DNS records). It is recommended to pin only keys you control (or really trust). Note that third-party CAs may change their keys and do less validations when signing new certificates for known hostnames (CNs).");
+    if(pi_samekey)
+        this.warnings.push("Multiple certificates/CSRs contain the same public key. This should be caused by user error (e.g. pasting single PEM two times).");
+    if(this.pins.length<2)
+        this.warnings.push("At least two pins (one of them must be backup pin - pin of securely held offline backup key) are required by the standard.");
+    if(this.pins.length<3)
+        this.warnings.push("Three or more pins (two or more backup key pins - pins of securely held offline backup keys) should be considered.");
+}
+
+/**
+ * Creates/formats report into displayable string.
+ *
+ * @param formatString string with template.
+ *   This string is repeated for each certificate/CSR
+ *   replacing all instances of "{Input}" (without quotes) with object from which public key was extracted,
+ *   replacing all instances of "{Type}" (without quotes) with type of object from which public key was extracted ("Certificate" or "CSR"),
+ *   replacing all instances of "{PublicKey}" (without quotes) with public key in PEM format,
+ *   replacing all instances of "{CN}" (without quotes) with Common Name,
+ *   replacing all instances of "{SHA256Hex}" (without quotes) with SHA256 hash in hex format of public key,
+ *   replacing all instances of "{SHA256}" (without quotes) with SHA256 hash in Base64 format of public key.
+ * @return string with formatted report.
+ */
+pkps.publicKeyPinsSet.prototype.formatReport = function(formatString) {
+    var i;
+    var ret = "";
+
+    for(i=0; i<this.pins.length; i++) {
+        ret += formatString
+                 .replace(/\{Input\}/i, this.inputs[i])
+                 .replace(/\{Type\}/i, this.pins[i].getType())
+                 .replace(/\{PublicKey\}/i, this.pins[i].getPublicKeyPem())
+                 .replace(/\{CN\}/i, this.pins[i].getCN())
+                 .replace(/\{SHA256Hex\}/i, this.pins[i].getPublicKeyHash("sha256", true))
+                 .replace(/\{SHA256\}/i, this.pins[i].getPublicKeyHash("sha256"));
+    }
+    return ret;
+}
+
+/**
+ * Checks whether publicKeyPinsSet has any warnings.
+ *
+ * @return boolean whether publicKeyPinsSet has warnings.
+ */
+pkps.publicKeyPinsSet.prototype.hasWarnings = function() {
+    return this.warnings.length>0;
+}
+
+/**
+ * Formats and concatenates warnings into displayable string.
+ *
+ * @param formatString string with template.
+ *   This string is repeated for each warning
+ *   replacing all instances of "{Warning}" (without quotes) with corresponding warning.
+ * @return string with formatted and concatenated warnings.
+ */
+pkps.publicKeyPinsSet.prototype.formatWarnings = function(formatString) {
+    var i;
+    var ret = "";
+
+    for(i=0; i<this.warnings.length; i++) {
+        ret += formatString.replace(/\{Warning\}/i, this.warnings[i]);
+    }
+    return ret;
+}
+
+/**
+ * Generates/formats value of Public-Key-Pins HTTP header.
+ *
+ * @param maxAge integer value of max-age directive.
+ * @param includeSubDomains boolean whether to add includeSubDomains directive.
+ * @return string with value of Public-Key-Pins HTTP header.
+ * @throws string with textual description of failure.
+ */
+pkps.publicKeyPinsSet.prototype.formatPKPHeader = function(maxAge, includeSubDomains) {
+    // input validation
+    if(typeof(maxAge)!=="number" || maxAge.match(/^[0-9]+$/)==null || typeof(includeSubDomains)!=="boolean")
+        throw "getPKPHeader() called with incorrect parameter(s)";
+
+    var i;
+    var ret = "";
+
+    for(i=0; i<this.pins.length; i++) {
+        ret += "pin-sha256=\""+this.pins[i].getPublicKeyHash("sha256")+"\"; ";
+    }
+    ret += "max-age="+maxAge;
+    if(includeSubDomains)
+        ret += "; includeSubDomains";
+    return ret;
+}
+
+/**
+ * Generates public key hashes for specified certificate or CSR.
  * Currently only RSA keys are supported.
  * Before calling this method, pkps.doSelfTests() must have been called.
  *
  * @param certificateOrCsr certificate or certificate signing request in PEM format.
- * @return publicKeyPin object with the hashes.
+ * @return publicKeyPin object with the hashes (and other data).
  *   getPublicKeyHash() and getPublicKeyPem() methods should be used on this object.
  * @throws string with textual description of failure.
  */
@@ -61,7 +206,7 @@ pkps.publicKeyPin = function(certificateOrCsr) {
         }
     }
     catch(e) {
-        throw "Unable to decode input.";
+        throw "Unable to decode certificate/CSR.";
     }
 
     try {
@@ -82,7 +227,7 @@ pkps.publicKeyPin = function(certificateOrCsr) {
     }
 
     try {
-        // extract Nommon Name
+        // extract Common Name
         this.cn = cert.subject.getField("CN").value;
         if(typeof(this.cn)!=="string" || this.cn.length<1)
             throw 1;
@@ -90,7 +235,6 @@ pkps.publicKeyPin = function(certificateOrCsr) {
     catch(e) {
         throw "Unable extract Common Name.";
     }
-
 
     try {
         // generate hashes
@@ -104,7 +248,7 @@ pkps.publicKeyPin = function(certificateOrCsr) {
     }
 }
 
-// used internally - generates and stores a hash
+// for internal use only - generates and stores a hash
 pkps.publicKeyPin.prototype.addHash = function(hashType, forgeMd, dSize) {
     var md = forgeMd.create();
     if(typeof(this.pk)!=="string" || this.keysize!==this.pk.length) // check key length
@@ -119,7 +263,7 @@ pkps.publicKeyPin.prototype.addHash = function(hashType, forgeMd, dSize) {
         throw 2;
 }
 
-// used internally - self-test certificate and CSR
+// for internal use only - self-test certificate and CSR
 pkps.self_tests_crtPEM = "-----BEGIN CERTIFICATE-----\r\nMIIDajCCAlICCQCOMMZ0DNdT0zANBgkqhkiG9w0BAQUFADB3MQswCQYDVQQGEwJY\r\nWDEVMBMGA1UEBwwMRGVmYXVsdCBDaXR5MRwwGgYDVQQKDBNEZWZhdWx0IENvbXBh\r\nbnkgTHRkMTMwMQYDVQQDDCpodHRwczovL2dpdGh1Yi5jb20vRGF2aXNOVC9QdWJs\r\naWMtS2V5LVBpbnMwHhcNMTQwNTMwMjEyMDU3WhcNMTUwNTMwMjEyMDU3WjB3MQsw\r\nCQYDVQQGEwJYWDEVMBMGA1UEBwwMRGVmYXVsdCBDaXR5MRwwGgYDVQQKDBNEZWZh\r\ndWx0IENvbXBhbnkgTHRkMTMwMQYDVQQDDCpodHRwczovL2dpdGh1Yi5jb20vRGF2\r\naXNOVC9QdWJsaWMtS2V5LVBpbnMwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK\r\nAoIBAQC3LSDIUB6UE4je4wBVOH1fYTdxOrV851L1vCLwzrmzwDd0nL4SYJ6BjJBt\r\n6V6XeAlAfsD19qBf4AIso9vAPRe4GX4EMirefOp3o8d94YIlEW2I1ueXZhwrRYWR\r\ncD1EIDmt11cHVHorfEzhc24GBjKZXP2lyOJzCN9iSQnVSs46/NpAyB0GhW6bUE6E\r\nBSMnQP2SvV89L7yeqtcsOBHp2Jb9xH63c0ujb9XMxIBqoCdSDz+K+rtE/VfVOC53\r\nwf3p3RZ5I1reSfMCzKoiEkNf/0GFmddhW/yIp2UbPXoy9AXmsLLjJlrfzmbeER2e\r\nr6v/6cG3prwRhCBDORaHkM6MQtwTAgMBAAEwDQYJKoZIhvcNAQEFBQADggEBAFMd\r\nuIzwwmQAtkB1wHpa9Xk/9Z6Hy6zj2QIsrwyx6gaxiTLRHfWok8UhLg0/LYfqCRnj\r\nrNqiGDlljBFIdznge7aVrc5rBNbk4wmVMsnHATxOsR2VE9GwVO7ahlVt+Oz5eTUl\r\n19Z0HYlkS2iFF8QrKiShqNNHKTKzNGBSj6aIhAnkflF1nIzoG/pvsDfdZmbkNNEW\r\n9jV6c3MrZt5tWSLULcodp6XguSLqyNceEqhmSUCDcMxdPPlhIWRU2S/HyGBWY5+N\r\nYUzjvAo5H5I7lZt/wSB5f4KAcw+hlyk6S1mv7U0JkA6fVV2ZeAPlDjYduK4aAgLk\r\nCqOCYoTPWdas6HWkrFo=\r\n-----END CERTIFICATE-----\r\n";
 pkps.self_tests_csrPEM = "-----BEGIN CERTIFICATE REQUEST-----\r\nMIICvDCCAaQCAQAwdzELMAkGA1UEBhMCWFgxFTATBgNVBAcMDERlZmF1bHQgQ2l0\r\neTEcMBoGA1UECgwTRGVmYXVsdCBDb21wYW55IEx0ZDEzMDEGA1UEAwwqaHR0cHM6\r\nLy9naXRodWIuY29tL0RhdmlzTlQvUHVibGljLUtleS1QaW5zMIIBIjANBgkqhkiG\r\n9w0BAQEFAAOCAQ8AMIIBCgKCAQEAty0gyFAelBOI3uMAVTh9X2E3cTq1fOdS9bwi\r\n8M65s8A3dJy+EmCegYyQbelel3gJQH7A9fagX+ACLKPbwD0XuBl+BDIq3nzqd6PH\r\nfeGCJRFtiNbnl2YcK0WFkXA9RCA5rddXB1R6K3xM4XNuBgYymVz9pcjicwjfYkkJ\r\n1UrOOvzaQMgdBoVum1BOhAUjJ0D9kr1fPS+8nqrXLDgR6diW/cR+t3NLo2/VzMSA\r\naqAnUg8/ivq7RP1X1Tgud8H96d0WeSNa3knzAsyqIhJDX/9BhZnXYVv8iKdlGz16\r\nMvQF5rCy4yZa385m3hEdnq+r/+nBt6a8EYQgQzkWh5DOjELcEwIDAQABoAAwDQYJ\r\nKoZIhvcNAQEFBQADggEBAH9PQ9eo+1ASvE27LHqJGJeAlY+aAI8VYMytod9yWBVl\r\ngwkoL97BxI28ZcWdQj3jWU1nYXGRUt6W4R/Z6UVB1YLKZ+4dYSaawDI3BdEwfqPt\r\nLeLxshgBf9f3NOzQa5OpyOlpXZzP9H9Fo9EYj6IpYLKPZaQX5t8/9/c3fpVbcYD/\r\neSl7GWUhOwK5KqpqYySuyL4I8y4BbzSvoouGUpGU+IghRShqI+rqnOPUuhNy14g2\r\nd5tMIkJJOg0Y124kpMcbDtsFKfKaRKm1NP+TvUN/WINK6DrMZc5wDJsrf2TiErAp\r\nC1icgpEM3snczBZPBG0cmJPOc72tHJM0/do31M864WM=\r\n-----END CERTIFICATE REQUEST-----\r\n";
 
@@ -155,6 +299,7 @@ pkps.publicKeyPin.prototype.getCN = function() {
  *
  * @param hashType the hash type to return.
  *   Allowed values are "sha1", "sha256", "sha384" and "sha512".
+ *   NB! Not all of these hash types may be used in Public-Key-Pins header.
  * @param hex specifies to return hash in hex format.
  *   Boolean true must be specified or this parameter must be omitted.
  * @return string with public key hash either in Base64 or hex format.
